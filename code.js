16318a54ff0a225c03b1d53c1c79f39ef87f62b2
Jobrien0394 <84590243+Jobrien0394@users.noreply.github.com>
2021-09-29 21:47:22 -0400

    Need help with this one as well.

--- code.js
+++ code.js
@@ -3,16 +3,22 @@ const coin = {
   flip: function () {
     // 1. Randomly set your coin object's "state" property to be either
     //    0 or 1: use "this.state" to access the "state" property on this object.
+    const coinFlip = Math.round(Math.random()) + 1;
   },
   toString: function () {
     // 2. Return the string "Heads" or "Tails", depending on whether
     //    "this.state" is 0 or 1.
+     0 = tails
+     1 = Head
+
   },
   toHTML: function () {
     const image = document.createElement("img");
     // 3. Set the properties of this image element to show either face-up
     //    or face-down, depending on whether this.state is 0 or 1.
     //    You can use the heads and tails images inside of the "images" folder
+    
+    
     return image;
   },
 };
